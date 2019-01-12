810cd2f828ae94ef8f4ac029fe21c97a0273120b
alex <alex.iv1102@gmail.com>
2019-01-12 10:49:58 +0200

    final release

--- js/js.js
+++ js/js.js
@@ -6,38 +6,74 @@ const IMAGES = [
 ];
 
 var gameContainer = document.getElementById('game-container');
-
-showCards();
+var firstCard;
+var firstCardNode;
+var secondCard;
+var secondCardNode;
+var winCount = 0;
 
 function showCards() {
     let doubleImages = IMAGES.concat(IMAGES);
     shuffle(doubleImages);
 
     doubleImages.forEach(element => {
-        // console.log(element);
         let card = document.createElement('div');
-        card.classList.add('flip-card');
-        let item = element.split('/')[1];
-        card.dataset.item = item;
-        card.innerHTML = `<div class="flip-card-front"></div>
-                                <div class="flip-card-back">
+        card.classList.add('card');
+        card.dataset.item = element;
+        card.innerHTML = `<div class="card-front"></div>
+                                <div class="card-back">
                                 <img class="image" src="${element}">
                             </div>`;
         gameContainer.appendChild(card);
     });
 }
 
-gameContainer.addEventListener('click', flipCard);
-
 function flipCard(event) {
-
-    console.log(event.target);
+    if (event.target.classList.contains('card-success')) return;
+    if (event.target.classList.contains('flip')) return;
 
     event.target.classList.add('flip');
+
+    if (!firstCard) {
+        firstCard = event.target.dataset.item;
+        firstCardNode = event.target;
+    } else {
+        secondCard = event.target.dataset.item;
+        secondCardNode = event.target;
+        checkPair();
+    }
 }
 
+function checkPair() {
+    if (firstCard === secondCard) {
+        setTimeout(function () {
+            firstCardNode.classList.replace('card', 'card-success');
+            secondCardNode.classList.replace('card', 'card-success');
+        }, 600);
+        checkWin();
+    } else {
+        setTimeout(returnFlipCard, 700);
+    }
+    firstCard = '';
+    secondCard = '';
+}
+
+function returnFlipCard() {
+    firstCardNode.classList.remove('flip');
+    secondCardNode.classList.remove('flip');
+}
+
+function checkWin() {
+    winCount++;
+    if (winCount === 4) {
+        setTimeout(function() {alert('You win!')}, 900);
+    }
+}
 
 function shuffle(o) {
     for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 };
+
+showCards();
+gameContainer.addEventListener('click', flipCard);
